63f317c4a8223211f27cb5a8c694c3abf672672c
Ryan Baer <ryanjbaer@outlook.com>
2023-02-28 20:40:44 -0700

    set up useEffect for api call

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,23 @@
-import './App.css';
 import { useEffect, useState } from 'react';
+import fetchData from '../../ApiCalls';
+import './App.css';
 
 function App() {
   const [items, setItems] = useState([])
-  const [loading, setLoading] = useState('')
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
 
-  
+  useEffect(() => {
+    fetchData()
+      .then(data => {
+        setItems(data)
+        setLoading(false)
+        console.log(items)
+      })
+      .catch(error => {
+        setError(error)
+      })
+  }, [])
 
   return (
     <div>
