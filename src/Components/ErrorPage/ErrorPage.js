fcb7fec76af25673c28e2d44f002297d54928462
Ryan Baer <ryanjbaer@outlook.com>
2023-03-02 15:51:18 -0700

    add error page and styling

---
+++ src/Components/ErrorPage/ErrorPage.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+import GameOver from "../../Images/GameOver.png"
+import "./ErrorPage.css"
+
+const ErrorPage = ({error}) => {
+    return (
+        <div className="ErrorPage">
+            <p className="error-text">{error}</p>
+            <Link to={"/"}>
+                <button className="error-return-button">Return to Main Page</button>
+            </Link>
+            <img src={GameOver} alt="Game Over"/>
+        </div>
+    )
+}
+
+export default ErrorPage
