732f11dd8f9b642159b8cfe3c05da088c2ed348f
Ryan Baer <ryanjbaer@outlook.com>
2023-03-05 09:18:07 -0700

    add datalist to form for dropdown

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -2,31 +2,42 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./SearchForm.css"
 
-const SearchForm = () => {
-    const [input, setInput] = useState('');
+const SearchForm = ({ itemNames }) => {
+  const [input, setInput] = useState('');
 
-    return (
-        <div className="SearchForm">
-            <form className="form">
-                <label className="search-label">Search Items</label>
-                <div>
-                    <input
-                        className="search-input" 
-                        title="search"
-                        value={input}
-                        onChange={(event) => setInput(event.target.value)}
-                        required/>
-                    <Link to={`/item/${input}`}>
-                        <input
-                            className="submit-input" 
-                            type="submit" 
-                            value="Search"
-                        />
-                    </Link>
-                </div>
-            </form>
+  // filter item names based on the input value
+  const filteredItems = itemNames.filter(itemName => 
+    itemName.toLowerCase().includes(input.toLowerCase())
+  );
+
+  return (
+    <div className="SearchForm">
+      <form className="form">
+        <label className="search-label">Search Items</label>
+        <div>
+          <input
+            className="search-input"
+            title="search"
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            list="items"
+          />
+          <datalist id="items">
+            {filteredItems.map((item, index) => (
+              <option key={index} value={item} />
+            ))}
+          </datalist>
+          <Link to={`/item/${input}`}>
+            <input
+              className="submit-input"
+              type="submit"
+              value="Search"
+            />
+          </Link>
         </div>
-    )
+      </form>
+    </div>
+  )
 }
 
 export default SearchForm;
